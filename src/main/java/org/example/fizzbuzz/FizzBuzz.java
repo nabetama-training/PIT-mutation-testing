7ec41e467ee9051d088a3d07f37333570d23f029
nabetama <mao.nabeta@gmail.com>
2021-04-09 20:23:41 +0900

    FizzBuzz

--- src/main/java/org/example/fizzbuzz/FizzBuzz.java
+++ src/main/java/org/example/fizzbuzz/FizzBuzz.java
@@ -3,6 +3,16 @@ package org.example.fizzbuzz;
 public class FizzBuzz {
 
     public String fizzBuzz(int n) {
+        if (n % 15 == 0) {
+            return "FizzBuzz";
+        }
+        if (n % 3 == 0) {
+            return "Fizz";
+        }
+        if (n % 5 == 0) {
+            return "Buzz";
+        }
+
         return String.valueOf(n);
     }
 }
--- src/test/java/org/example/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/org/example/fizzbuzz/FizzBuzzTest.java
@@ -1,6 +1,11 @@
 package org.example.fizzbuzz;
 
-import org.junit.jupiter.api.Test;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+import org.junit.jupiter.api.*;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -9,5 +14,65 @@ class FizzBuzzTest {
     void parrotTest() {
         FizzBuzz fb = new FizzBuzz();
         assertEquals("1", fb.fizzBuzz(1));
+    }
+
+    @Test
+    void returnFizzTest() {
+        FizzBuzz fb = new FizzBuzz();
+        assertEquals("Fizz", fb.fizzBuzz(3));
+        assertEquals("Fizz", fb.fizzBuzz(6));
+    }
+
+    @Test
+    void returnBuzzTest() {
+        FizzBuzz fb = new FizzBuzz();
+        assertEquals("Buzz", fb.fizzBuzz(5));
+        assertEquals("Buzz", fb.fizzBuzz(10));
+    }
+
+    @Test
+    void returnFizzBuzzTest() {
+        FizzBuzz fb = new FizzBuzz();
+        assertEquals("FizzBuzz", fb.fizzBuzz(15));
+        assertEquals("FizzBuzz", fb.fizzBuzz(30));
+    }
+
+    @Nested
+    class Range {
+        @AllArgsConstructor
+        @Getter
+        @Accessors(fluent = true)
+        class TestCase {
+            private final int inputValue;
+            private final String expected;
+        }
+
+        @TestFactory
+        Stream<DynamicNode> testFizzBuzz() {
+            return Stream.of(
+                    new TestCase(1, "1"),
+                    new TestCase(2, "2"),
+                    new TestCase(3, "Fizz"),
+                    new TestCase(4, "4"),
+                    new TestCase(5, "Buzz"),
+                    new TestCase(6, "Fizz"),
+                    new TestCase(7, "7"),
+                    new TestCase(8, "8"),
+                    new TestCase(9, "Fizz"),
+                    new TestCase(10, "Buzz"),
+                    new TestCase(11, "11"),
+                    new TestCase(12, "Fizz"),
+                    new TestCase(13, "13"),
+                    new TestCase(14, "14"),
+                    new TestCase(15, "FizzBuzz"),
+                    new TestCase(16, "16")
+            ).map(testCase -> DynamicTest.dynamicTest(
+                    "1から16までテスト",
+                    () -> {
+                        FizzBuzz fb = new FizzBuzz();
+                        assertEquals(testCase.expected(), fb.fizzBuzz(testCase.inputValue()));
+                    }
+            ));
+        }
     }
 }
